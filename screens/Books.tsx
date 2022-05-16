9075a4f4a4db3fa088575470b65aef83b3fd37fe
andreaberumen <aberumen@cetys.edu.mx>
2022-05-16 16:00:11 -0700

    test commit

--- screens/Books.tsx
+++ screens/Books.tsx
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
   bookCard: {
     flex: 1,
     backgroundColor: '#f2f2f2',
+    color: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
