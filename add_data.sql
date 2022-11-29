ac5b161230bebbdaed7a209d4a722c16937d22cc
Andrea Berumen <aberumen@cetys.edu.mx>
2022-11-29 13:44:33 -0800

    add data scripts

---
+++ add_data.sql
@@ -0,0 +1,251 @@
+ALTER TABLE libro
+ADD Libro_URL varchar(200);
+
+INSERT INTO libro
+VALUES (
+    'The Art of Money Getting'
+    ,'P. T. Barnum'
+    ,1880
+    ,'EN'
+    ,13611
+    ,'P. T. Barnum, the legendary entertainer and co-founder of the Barnum and Bailey Circus, was not just a successful businessman, but a philanthropist and writer as well. This short, pamphlet-length work distills Barnum’s advice on achieving success and wealth, in his own words.'
+    ,'https://standardebooks.org/ebooks/p-t-barnum/the-art-of-money-getting/text/single-page'
+),
+(
+    'The Book of Tea'
+    ,'Okakura Kakuzō'
+    ,1906
+    ,'EN'
+    ,18034
+    ,'The Book of Tea, one of the great English tea classics, is a long essay about the connection between teaism, Taoism, and the aesthetics of Japanese culture. It was written by Okakura Kakuzō in English and was published in the United States in 1906.
+
+The essay targets a Western audience and seeks to explain the importance of tea in Japanese culture, not just as a beverage, but as a form of art expressed in different aspects. After a brief introduction of the Western attitude towards tea, Okakura demystifies the admiration of the Japanese people for this green plant by presenting the different schools of tea, its connection to Zen philosophy, and how it has affected the arts. The famous tea ceremony and its rigid formalities are explained, together with the contributions of the great tea-masters.
+
+The Book of Tea is considered by many to be one of the first books to introduce Eastern culture and philosophy to the Western world. This was possible due to Okakura’s early contact with the English language and Western thought, but also due to his later involvement in the Asian art division of the Boston Museum of Fine Arts, which he came to head in 1910.'
+    ,'https://standardebooks.org/ebooks/okakura-kakuzo/the-book-of-tea/text/single-page'
+),
+(
+    'The Origin of Species'
+    ,'Charles Darwin'
+    ,1880
+    ,'EN'
+    ,200099
+    ,'The Origin of Species by Charles Darwin must rank as one of the most influential and consequential books ever published, initiating scientific, social and religious ferment ever since its first publication in 1859. Its full title is The Origin of Species by Means of Natural Selection, or the Preservation of Favoured Races in the Struggle for Life, in some editions prefaced by the word “On.”
+
+Darwin describes the book as simply an “abstract” of his ideas, which are more fully fleshed out and supported with detailed examples in his other, more scholarly works (for example, he wrote several long treatises entirely about barnacles). The Origin of Species itself was intended to reach a wider audience and is written in such a way that any reasonably educated and thoughtful reader can follow Darwin’s argument that species of animals and plants are not independent creations, fixed for all time, but mutable. Species have been shaped in response to the effects of natural selection, which Darwin compares to the directed or manual selection by human breeders of domesticated animals.
+
+The Origin of Species was eagerly taken up by the reading public, and rapidly went through several editions. This Standard Ebooks edition is based on the sixth edition published by John Murray in 1872, generally considered to be the definitive edition with many amendments and updates by Darwin himself.
+
+The Origin of Species has never been out of print and continues to be an extremely popular work. Later scientific discoveries such as the breakthrough of DNA sequencing have refined our concept of some of Darwin’s ideas and given us a better understanding of issues he found puzzling, but the basic thrust of his theory remains unchallenged.'
+    ,'https://standardebooks.org/ebooks/charles-darwin/the-origin-of-species/text/single-page'
+),
+(
+    'Emma'
+    ,'Jane Austen'
+    ,1815
+    ,'EN'
+    ,160838
+    ,'Emma is one of Jane Austen’s best-loved novels. Its eponymous heroine, Emma Woodhouse, has lived a pampered, protected life and consequently is somewhat unrealistic when she sets her sights on becoming a matchmaker for a young friend.
+
+The novel provides a light-hearted insight into the distinctions of the rigid class structure of England in the Regency period, and the social barriers to marriage between persons considered to be of superior and inferior rank.
+
+Emma was published in 1815, the last of Austen’s novels to be published while she still lived. It received a generally very positive reception, and was well reviewed (though anonymously) by Sir Walter Scott. Criticisms of the novel, such as they were, centered around its supposed lack of plot, though its treatment of character was recognized and applauded. Today it is regarded as one of Austen’s best works. The novel has been adapted many times for theater, movies and television.'
+    ,'https://standardebooks.org/ebooks/jane-austen/emma/text/single-page'
+),
+(
+    'Wuthering Heights'
+    ,'Emily Brontë'
+    ,1847
+    ,'EN'
+    ,13611
+    ,'Returning from Liverpool, Mr. Earnshaw brings with him a dirty, ragged, black-haired child called Heathcliff, and sets into motion a tale of destructive passions. The book’s two locations, the genteel Thrushcross Grange and the wild Wuthering Heights, serve as matching backgrounds to the characters of their occupants, as they struggle to gain the upper hand in marriage and power. All the while, the ghosts of the past seem to drive revenge more than inspire forgiveness.
+
+Wuthering Heights was Emily Brontë’s sole published novel before her early death at the age of 30. Published under the pen name of Ellis Bell, a shared surname with the pen names of her sisters, many assumed that such a book could only have been written by a man. Reviewers of the time praised its emotional power but were also shocked at the actions of its characters, and most agreed that it was impossible to put down. After the novel’s original publication in 1847 it was revised into a single volume in 1850, and over time has become a classic of English literature. The story has been reworked into plays, operas, films, TV dramatisations and a ballet, and has inspired many further works of art, music and literature.'
+    ,'https://standardebooks.org/ebooks/emily-bronte/wuthering-heights/text/single-page'
+),
+(
+    'Journals'
+    ,'Alexander Mackenzie'
+    ,1802
+    ,'EN'
+    ,121005
+    ,'Alexander Mackenzie was the first European to complete a land crossing of the continent of America north of Mexico, preceding the famous Lewis and Clark expedition by twelve years. In his journals he details two separate voyages: one up what is now known as the Mackenzie River to the Arctic Ocean in 1789, and another to what is now Bella Coola on the Pacific Ocean in 1792 and 1793.
+
+Both journals provide a detailed description of the many difficulties in navigating and traveling in a country that had yet to be mapped. Having to rely on Native guides and rumors, and enduring hardships that almost beggar belief, Mackenzie and his team were able to achieve their objective of finding an east to west land crossing through the Rocky Mountains and to the Pacific Ocean. Although his route didn’t prove as practical as routes found by later explorers, Mackenzie has cemented himself as a key explorer of Western Canada.'
+    ,'https://standardebooks.org/ebooks/alexander-mackenzie/journals/text/single-page'
+),
+(
+    'The Voyages of Doctor Dolittle'
+    ,'Hugh Lofting'
+    ,1922
+    ,'EN'
+    ,13611
+    ,'After discovering an injured squirrel, young Tommy Stubbins is introduced to Doctor Dolittle. Enamored by the Doctor’s ability to speak with animals and his wonderful house full of creatures from around the world, Tommy convinces the Doctor to take him as an apprentice. Together with Polynesia the parrot, Jip the dog, and an African prince named Bumpo, they set out in search of the famed naturalist Long Arrow, son of Golden Arrow, who has gone missing.
+
+This is the second book in Hugh Lofting’s Doctor Dolittle series. It was awarded the 1923 Newbery Medal.'
+    ,'https://standardebooks.org/ebooks/hugh-lofting/the-voyages-of-doctor-dolittle/text/single-page'
+),
+(
+    'Winnie-the-Pooh'
+    ,'A. A. Milne'
+    ,1926
+    ,'EN'
+    ,23270
+    ,'Winnie-the-Pooh is a bear that likes honey perhaps a little too much and lives in the Hundred Acre Wood with his animal friends Rabbit, Piglet, Owl, Eeyore, Kanga, and Roo, as well as his people friend Christopher Robin. Winnie-the-Pooh contains several stories of adventures involving Pooh and his friends, including a birthday party, looking for heffalumps, finding a missing tail, and playing a trick on one of their own. Most of them, of course, also involve honey in one way or another.
+
+A. A. Milne wrote for Punch magazine, authored a detective novel (The Red House Mystery), and published several plays, but all of them were largely forgotten after he began writing children’s books about his son’s stuffed toys. Winnie-the-Pooh and his friends captured the public’s imagination, and though Milne was only to publish four books of their adventures, they have lived on in the imagination of children ever since.'
+    ,'https://standardebooks.org/ebooks/a-a-milne/winnie-the-pooh/text/single-page'
+),
+(
+    'The Picture of Dorian Gray'
+    ,'Oscar Wilde'
+    ,1890
+    ,'EN'
+    ,79767
+    ,'The Picture of Dorian Gray was first published as a serial in Lippencott’s Monthly Magazine, and the publishers thought it would so offend readers that they removed nearly 500 words without Wilde’s approval. Wilde soon expanded it and republished it as a novel, including a short preface justifying his art. Even though his contemporaries considered it so offensive that some argued for his prosecution, Dorian Gray today survives as a classic philosophical novel that explores themes of aestheticism and double lives. Couched in Wilde’s trademark cutting wit, Dorian Gray is still being adapted today, with Dorian and his moldering portrait remaining cultural touchstones.'
+    ,'https://standardebooks.org/ebooks/oscar-wilde/the-picture-of-dorian-gray/text/single-page'
+),
+(
+    'The Pilgrim’s Progress'
+    ,'John Bunyan'
+    ,110131
+    ,'EN'
+    ,13611
+    ,'The Pilgrim’s Progress from This World, to That Which Is to Come was written in 1678 by John Bunyan, a Puritan and a dissenter from the Church of England. It is an allegory of the journey to redemption of the faithful, through many snares and difficulties. Cast in the form of a dream, the first part of the work deals with a man called Christian, who sets off carrying a great burden. He meets many helpers and many adversaries on this journey. The second part of the work deals with Christian’s wife, Christiana, and her four children, who follow a similar journey.
+
+One of the most influential of all religious works, The Pilgrim’s Progress was immediately popular and has been translated over the years into many languages and into many forms, including verse, opera, movies, and many illustrated versions for children. Several of its story elements, characters and locations have entered the language, such as the “Slough of Despond,” “Vanity Fair,” “Great-heart,” and “Giant Despair.”
+
+This edition is based on a version of Bunyan’s complete works edited by George Offor and published in 1855. It contains many endnotes drawn from a variety of commentators.'
+    ,'https://standardebooks.org/ebooks/john-bunyan/the-pilgrims-progress/text/single-page'
+),
+;
+
+INSERT INTO coleccion
+VALUES (
+    'BBC’s 100 Greatest British Novels'
+    ,'In search of a collective critical assessment, BBC Culture contributor Jane Ciabattari polled 82 book critics, from Australia to Zimbabwe – but none from the UK. This list includes no nonfiction, no plays, no narrative or epic poems (no Paradise Lost or Beowulf), no short story collections (no Morte D’Arthur) – novels only, by British authors (which means no James Joyce).'
+),
+(
+    'Guardian’s Best 100 Novels in English'
+    ,'After two years of careful consideration, Robert McCrum has reached a verdict on his selection of the 100 greatest novels written in English.'
+),
+(
+    'Encyclopædia Britannica’s Great Books of the Western World'
+    ,'Great Books of the Western World is a series of books originally published in the United States in 1952, by Encyclopædia Britannica, Inc., to present the great books in a 54-volume set.'
+);
+
+INSERT INTO genero(Nombre_Genero)
+VALUES (
+    'fiction'
+),
+(
+    'spirituality'
+),
+(
+    'adventure'
+),
+(
+    'nonfiction'
+),
+(
+    'travel'
+),
+(
+    'children’s'
+);
+
+INSERT INTO genero_libro
+VALUES (
+    1
+    ,4
+),
+(
+    2
+    ,4
+),
+(
+    3
+    ,4
+),
+(
+    4
+    ,1
+),
+(
+    5
+    ,1
+),
+(
+    6
+    ,3
+),
+(
+    6
+    ,4
+),
+(
+    6
+    ,5
+),
+(
+    7
+    ,1
+),
+(
+    7
+    ,3
+),
+(
+    7
+    ,6
+),
+(
+    8
+    ,6
+),
+(
+    9
+    ,1
+),
+(
+    10
+    ,1
+),
+(
+    10
+    ,2
+);
+
+INSERT INTO coleccion_libro
+VALUES (
+    3
+    ,3
+),
+(
+    4
+    ,1
+),
+(
+    4
+    ,2
+),
+(
+    4
+    ,3
+),
+(
+    5
+    ,1
+),
+(
+    5
+    ,2
+),
+(
+    9
+    ,2
+),
+(
+    10
+    ,2
+);
---
+++ add_data_2.sql
@@ -0,0 +1,226 @@
+INSERT INTO libro
+VALUES (
+    'His Masterpiece'
+    ,'Émile Zola'
+    ,1885
+    ,'EN'
+    ,146102
+    ,'His Masterpiece, sometimes translated as “The Work” or “The Masterpiece,” is Zola’s 14th entry in his Rougon-Macquart series of novels. In it we see Claude Lantier, a painter with obvious talent, struggle to leave a revolutionary mark on the art world of 19th-century Paris. The novel deftly explores the themes of genius, poverty, purity in art, art as a bureaucratic institution, obsession, and madness.
+
+The book is notable not just for its accurate portrayal of the art world of the time, but also for the interesting personal details Zola incorporated into the book. Lantier is a pastiche of several famous painters Zola personally knew, including Paul Cézanne, Claude Monet, and Édouard Manet; Lantier’s masterpiece is based on Manet’s revolutionary painting Le Déjeuner sur l’herbe; and the novel’s accuracy is even blamed on ending the long friendship between Zola and Cézanne. Zola himself includes a self-portrait, as the character Pierre Sandoz.
+
+Vizetelly’s translation is fresh and readable, and Zola’s rendition of Paris and the surrounding countryside is vibrant and engrossing. Rarely do we get such a close and engaging window into bohemian life in old Paris.'
+    ,'https://standardebooks.org/ebooks/emile-zola/his-masterpiece/ernest-alfred-vizetelly/text/single-page'
+),
+(
+    'Alice’s Adventures in Wonderland'
+    ,'Lewis Carroll'
+    ,1865
+    ,'EN'
+    ,27388
+    ,'Everyone’s familiar with Lewis Carroll’s famous children’s classic and the huge influence it’s had on modern culture. The Mad Hatter, the Duchess, the Cheshire Cat, the White Rabbit, the Caterpillar—all of these strange and familiar creatures appear in Alice, and the nonsensical style, memorable poems, and colorful set pieces are just as influential.
+
+The book was originally written as gift to Alice Liddel, the daughter of one of Carroll’s friends. Carroll had told the story to Alice during a rainy-day boat trip up a river. It stuck in his mind, and a few years later the completed manuscript was published in 1865. More than 150 years later, it’s just as popular as it ever was.'
+    ,'https://standardebooks.org/ebooks/lewis-carroll/alices-adventures-in-wonderland/john-tenniel/text/single-page'
+),
+(
+    'Meditations'
+    ,'Marcus Aurelius'
+    ,170
+    ,'EN'
+    ,45696
+    ,'Roman Emperor Marcus Aurelius Antoninus Augustus was the model of what we call a philosopher-king. Though his rule was troubled by war and conflict, he remained a thoughtful and even-handed ruler.
+
+Meditations isn’t a complete book, but rather a collection of his personal diary entries written over a ten-year campaign in Greece. The entries were never meant to be published; instead, they were a reminder to himself of how to remain calm, tranquil, and kind, even in the worst of situations. In them we see the emperor working out how to deal with the everyday problems all of us face: annoying coworkers, difficult family members, the expectations of others, unrealized goals and achievements, and, ultimately, happiness.
+
+The episodic nature of Meditations makes it hard to follow at times, but in exchange we get a deeply personal window into the life of one of Rome’s most unique emperors, and more importantly, a handbook of thoughtful advice on how to live a tranquil, satisfied, and productive life.
+
+'
+    ,'https://standardebooks.org/ebooks/marcus-aurelius/meditations/george-long/text/single-page#colophon'
+),
+(
+     'The Adventures of Huckleberry Finn'
+    ,'Mark Twain'
+    ,1884
+    ,'EN'
+    ,115483
+    ,'One of the great American novels, The Adventures of Huckleberry Finn tells the story of Huck Finn and his travels with Jim, an escaped slave. Roundly criticised by contemporary reviewers for its colorful and literal language and even banned by several libraries, it sealed its historical importance in part by being one of the first novels to be written entirely in American vernacular.
+
+While Huck Finn is, on its face, an adventure tale for younger readers, it’s also a cutting satire and a nuanced examination of racism and morality. Hemingway called it “the best book we’ve had.”'
+    ,'https://standardebooks.org/ebooks/mark-twain/the-adventures-of-huckleberry-finn/text/single-page'
+),
+(
+    'Beyond Good and Evil'
+    ,'Friedrich Nietzsche'
+    ,1886
+    ,'EN'
+    ,63623
+    ,'Beyond Good and Evil, one of Nietzsche’s four “late period” works, is a philosophical treatise organized into nine parts and 296 short individual sections. In it he explores the concept of morality as taken for granted by contemporary philosophers, and whether “good” and “evil” should be considered just two sides of the same coin.'
+    ,'https://standardebooks.org/ebooks/friedrich-nietzsche/beyond-good-and-evil/helen-zimmern/text/single-page'
+),
+(
+    'A Confession'
+    ,'Leo Tolstoy'
+    ,1882
+    ,'EN'
+    ,25992
+    ,'Leo Tolstoy wrote this short meditation on sadness and the meaning of life when he was middle aged. He had already completed his masterworks, Anna Karenina and War and Peace, reared fourteen children, and gained fame and acclaim in Russia as a man of letters. But despite having attained that success, he still found himself unhappy and always returning to the disturbing idea that all achievement is meaningless.
+
+A Confession is his attempt to put these thoughts in words as he teetered on the brink of suicide. It forms the first in a four-volume series that included A Criticism of Dogmatic Theology, The Gospel in Brief, and What I Believe (also known as My Religion or My Faith).'
+    ,'https://standardebooks.org/ebooks/leo-tolstoy/a-confession/aylmer-maude/text/single-page'
+),
+(
+    'Siddhartha'
+    ,'Hermann Hesse'
+    ,1922
+    ,'EN'
+    ,39672
+    ,'Hermann Hesse wrote Siddhartha during a period in his life in which he suffered what he described as a “sickness with life.” He claimed to be unable to complete the book because he had not experienced the kind of nirvana that Siddhartha, the main character, wants to achieve—so Hesse surrounded himself with sacred Buddhist and Hindu teachings and lived as a recluse in order to complete this work.
+
+Siddhartha is a short, simple tale of a man’s quest to achieve enlightenment and happiness. Over twelve short chapters the reader follows Siddhartha through his time as a young adult, to his exploration of spirituality as a traveling ascetic, to his delvings in lust, business, and greed, to his time as an old man. At each stage of his life Siddhartha yearns for nirvana, finally achieving it only after realizing that it’s all of life’s experiences that form it, not the teachings of any one man.
+
+Today Siddhartha remains an influential text in new Western spirituality.'
+    ,'https://standardebooks.org/ebooks/hermann-hesse/siddhartha/gunther-olesch_anke-dreher_amy-coulter_stefan-langer_semyon-chaichenets/text/single-page'
+),
+(
+    'Little Women'
+    ,'Louisa May Alcott'
+    ,1868
+    ,'EN'
+    ,190768
+    ,'Little Women is the classic coming-of-age tale of four sisters on their journey to adulthood. Though today it’s considered a classic, Little Women almost wasn’t written: Alcott wanted to publish a collection of short stories instead, but her publisher and her father pressed her to write a book that would appeal to a wide audience of young girls.
+
+The first volume was written quickly and published in 1868; it was a huge success, and Alcott composed the second volume just as quickly and published it in 1869. By her own account she didn’t enjoy writing them, and both she and her publisher agreed the first few chapters were dull—so it almost goes without saying she was pleasantly shocked at the positive reception the volumes received.
+
+By 1927 it had been acknowledged as one of the most widely-read novels of the era, and remains widely read today.'
+    ,'https://standardebooks.org/ebooks/louisa-may-alcott/little-women/text/single-page'
+),
+(
+    'Don Quixote'
+    ,'Miguel De Cervantes Saavedra'
+    ,1605
+    ,'EN'
+    ,426868
+    ,'Don Quixote is a novel that doesn’t need much introduction. Not only is it widely considered the greatest Spanish literary work of all time, one of the greatest literary works in history, and a cornerstone of the Western literary canon, it’s also considered one of the first—if not the first—modern novels.
+
+This Standard Ebooks edition is believed to be the first ebook edition of Don Quixote to feature a full transcription of translator John Ormsby’s nearly 1,000 footnotes. Ormsby as an annotator deftly explains obscure passages, gives background on the life and times of 1600s Spain, references decisions from other contemporary translators, and doesn’t hold back from sharing his views on the genius—and flaws—of Cervantes’ greatest work.
+
+The story is of the eponymous Don Quixote, a country noble who, in his old age, reads too many chivalric romances and goes mad. After convincing his grubby servant, Sancho Panza, to join him as his squire, he embarks on an absurd and comic quest to do good and right wrongs.
+
+Today Don Quixote’s two volumes are published as a single work, but their publication came ten years apart. Cervantes saw great success with the publication of his first volume, and appeared to have little desire to write a second volume until a different author wrote a spurious, inferior sequel. This kicked Cervantes into gear and he wrote volume two, a more serious and philosophical volume than the largely comic first volume.
+
+Despite being written in 1605 and translated in 1885, Don Quixote contains a surprising amount of slapstick laughs—even for the modern reader—and narrative devices still seen in today’s fiction, including meta-narratives, frame narratives, and metafiction. Many scenes (like Quixote’s attack on the windmills) and characters (like Sancho Panza and Lothario) are so famous that they’re ingrained in our collective culture.'
+    ,'https://standardebooks.org/ebooks/miguel-de-cervantes-saavedra/don-quixote/john-ormsby/text/single-page'
+),
+(
+    'Madame Bovary'
+    ,'Gustave Flaubert'
+    ,1857
+    ,'EN'
+    ,116539
+    ,'Madame Bovary, often ranked among the greatest novels of all time, is Flaubert’s first novel, and considered to be both his masterpiece and one of the most influential works in literary history, with authors from Henry James to Proust to Nabokov heaping it with praise.
+
+The novel tells the story of Emma Bovary, a commoner wife of a country doctor, and her attempts to escape the drudgery of day-to-day mediocrity by engaging in adulterous affairs and overspending on luxuries. She remains unsatisfied even though her husband adores her and they want for little, and her shallowness eventually leads to their ruin.
+
+The story was first serialized in Revue de Paris, where prosecutors tried to have it censored for obscenity, arguing that not only is the story immoral, but that realism as a literary style is an offence against art and decency. The trial only served to increase the story’s fame, and when it was published as a single novel it quickly became a bestseller.'
+    ,'https://standardebooks.org/ebooks/gustave-flaubert/madame-bovary/eleanor-marx-aveling/text/single-page'
+),
+;
+
+INSERT INTO genero(Genero_ID,Nombre_Genero)
+VALUES (
+    7
+    ,'philosophy'
+);
+
+INSERT INTO genero_libro
+VALUES (
+    11
+    ,1
+),
+(
+    12
+    ,1
+),
+(
+    12
+    ,6
+),
+(
+    13
+    ,7
+),
+(
+    14
+    ,1
+),
+(
+    14
+    ,6
+),
+(
+    15
+    ,7
+),
+(
+    16
+    ,2
+),
+(
+    16
+    ,7
+),
+(
+    17
+    ,1
+),
+(
+    17
+    ,2
+),
+(
+    18
+    ,1
+),
+(
+    19
+    ,1
+),
+(
+    20
+    ,1
+);
+
+INSERT INTO coleccion_libro
+VALUES (
+    11
+    ,1
+),
+(
+    11
+    ,2
+),
+(
+    13
+    ,3
+),
+(
+    14
+    ,2
+),
+(
+    14
+    ,3
+),
+(
+    15
+    ,3
+),
+(
+    18
+    ,2
+),
+(
+    19
+    ,3
+);
